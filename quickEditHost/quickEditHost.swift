c5bbebe5ca1afcd6ab6fe94039dc4b3cdcad9625
XGHeaven <xgheaven@gmail.com>
2016-10-20 20:21:59 +0800

    add status label

--- quickEditHost/quickEditHost.swift
+++ quickEditHost/quickEditHost.swift
@@ -17,6 +17,14 @@ class quickEditHost: NSPreferencePane {
     
     @IBOutlet weak var statusLabel: NSTextField!
     
+    enum ActionStatus {
+        case None
+        case SaveSuccess
+        case SaveFailed
+        case HostEdited
+        case HostLoaded
+    }
+    
     let hostPath = "/etc/hosts"
     
     override func mainViewDidLoad() {
@@ -24,12 +32,35 @@ class quickEditHost: NSPreferencePane {
         hostText.textStorage?.mutableString.setString(hosts)
         saveButton.target = self
         saveButton.action = #selector(self.saveHost(_:))
+        changeStatus(.HostLoaded)
     }
     
     @IBAction func saveHost(_ sender: AnyObject) {
-        if let newHost = (hostText.textStorage?.mutableString) as? String {
+        var errInfo:NSDictionary?
+        if let newHost:String = ((hostText.textStorage?.mutableString)! as NSString) as String? {
             let hosts = newHost.replacingOccurrences(of: "\n", with: "\\n")
-            NSAppleScript(source: "do shell script \"sudo echo -ne '\(hosts)' > /etc/hosts\" with administrator privileges")!.executeAndReturnError(nil)
+            NSAppleScript(source: "do shell script \"sudo echo -n '\(hosts)' > /etc/hosts\" with administrator privileges")!
+                .executeAndReturnError(&errInfo)
+            if errInfo != nil {
+                changeStatus(.SaveFailed)
+            } else {
+                changeStatus(.SaveSuccess)
+            }
+        }
+    }
+    
+    func changeStatus(_ status:ActionStatus) {
+        switch status {
+        case .None:
+            statusLabel.stringValue = ""
+        case .SaveSuccess:
+            statusLabel.stringValue = "✅ Save Success"
+        case .SaveFailed:
+            statusLabel.stringValue = "❌ Save Failed"
+        case .HostEdited:
+            statusLabel.stringValue = "Host Edited"
+        case .HostLoaded:
+            statusLabel.stringValue = "Host Loaded"
         }
     }
 }
